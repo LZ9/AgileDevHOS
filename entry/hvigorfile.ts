e121f7bab93a495515c5217a4badb82e35acc766
zhouL <lz.mms@foxmail.com>
2025-10-10 09:39:55 +0800

    添加鸿蒙路由插件配置

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,6 +1,7 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { appPlugin } from "@hadss/hmrouter-plugin";
 
 export default {
   system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [appPlugin({ ignoreModuleNames: [] })]       /* Custom plugin to extend the functionality of Hvigor. */
 }
